217cbdcf108aa45d337059fe401c870083aea3d1
Thesanga Lazarus <thesi.laz23@gmail.com>
2025-12-29 16:23:59 +0530

    Initial coursework React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import PropertyPage from "./pages/PropertyPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/property/:id" element={<PropertyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
